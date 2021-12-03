df6b5b0ea5ea71ae684764a9306d63968b5ff7a1
Fangfei <zu1024@qq.com>
2021-12-03 18:19:24 +0800

    1. 初次提交

---
+++ first.js
@@ -0,0 +1,45 @@
+/*const x = 1;
+let b = "hello, world";
+
+if (2 > 1) {
+    x = 1;
+    y = 2;
+}*/
+// 注释
+/*
+这是
+一些
+注释
+*/
+// alert('我想执行')
+// alert('我不想执行')
+
+var age = 15;
+if (age > 18) {
+    alert('成年人');
+} else {
+    alert('非成年人')
+}
+
+var person = {
+    name: 'Bob',
+    age: 20,
+    tags: ['js', 'web', 'mobile'],
+    city: 'Beijing',
+    hasCar: true,
+    zipcode: null
+};
+
+alert(person.name)
+
+var x = 100;
+console.log(x)
+
+var c;
+c = `反引号
+多行的
+挺不错的`
+
+var name = '小明';
+var age = 20;
+var message = ''
